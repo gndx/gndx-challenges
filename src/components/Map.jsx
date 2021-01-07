704a257107e985992f1aa90d5d8adf5e1480053c
David Urbina <duparra15@gmail.com>
2021-01-07 14:12:11 -0400

    Implementado servicio de notificación
    Suscribe un callback al servicio, actualizando la vista del mapa cada vez que el servicio notifica.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
 import { mapboxAPIAccessToken } from '../utils/credentials.js';
+import addressService from '../utils/addressService.js';
 
 class Map extends Component {
+
+    // Map is defined here so that it can be destroyed on unmount
+    map;
+    
     componentDidMount() {
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        this.map = L.map('map').setView([51.505, -0.09], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
@@ -12,9 +17,20 @@ class Map extends Component {
             tileSize: 512,
             zoomOffset: -1,
             accessToken: mapboxAPIAccessToken
-        }).addTo(map);
+        }).addTo(this.map);
+        
+        addressService.suscribe('map', mapData => {
+            let newPos = L.latLng(mapData.location.lat, mapData.location.lng);
+            this.map.setView(newPos, this.map.getZoom());
+        });
     };
+    
+    componentWillUnmount() {
+        this.map.remove();
+        addressService.unsuscribe('map');
+    }
 
+    // TODO: make map containter height adjustable to screen size
     render() {
         return(
             <div id="map" style={{height: '400px'}}></div>
