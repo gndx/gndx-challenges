970b69df7224a8f8631e9f7c09b62ab290533898
David Urbina <duparra15@gmail.com>
2021-01-07 19:06:04 -0400

    Renderizada barra de dirección

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx';
 import Map from './components/Map.jsx'
+import AddressBar from './components/AddressBar.jsx'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
           <Header/>
+          <AddressBar/>
           <Map/>
     </div>
   );
