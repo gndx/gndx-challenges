de4f58d3f16383b48f718ce534ac1f2c7ec19ab3
David Urbina <duparra15@gmail.com>
2021-01-09 16:50:17 -0400

    Componente refactorizado
    Cambiado de grid a flexbox.
    Eliminado div innecesario.
    Creados dividers
    Implementado font weight y size

--- src/components/AddressBar.jsx
+++ src/components/AddressBar.jsx
@@ -27,59 +27,57 @@ class AddressBar extends Component {
         if(!/Mobi/i.test(window.navigator.userAgent))
             return; 
 
-        e.currentTarget.classList.toggle('collapse')
         let first = true;
-        let children = e.currentTarget.children
-
-        for(let child of children){
+        for(let child of e.currentTarget.children){
             if(first){
                 first = false;
                 continue;
             }                
-            child.classList.toggle('collapse-item')
+            child.classList.toggle('collapse')
         }
     }
 
     render(){
         return(
             <section id={this.state.id}>
-                <div className="container">
-                    <div className="row" onClick={this.handleClick}>
-                        <div className="divider">
-                            <p>IP ADDRESS</p>
-                            {
-                                this.state.address &&
-                                <h3>{this.state.address.ip}</h3>
-                            }
-                        </div>
-                        <div className="divider">
-                            <p>LOCATION</p>
-                            {
-                                this.state.address &&
-                                this.state.address.location &&
-                                <h3>
-                                    {`${this.state.address.location.city}, `}
-                                    {/* {`${this.state.address.location.region}, `} */}
-                                    {`${this.state.address.location.country} `}
-                                    {this.state.address.location.postalCode}
-                                </h3>
-                            }
-                        </div>
-                        <div className="divider">
-                            <p>TIMEZONE</p>
-                            {
-                                this.state.address &&
-                                this.state.address.location &&
-                                <h3>{`UTC ${this.state.address.location.timezone}`}</h3>
-                            }
-                        </div>
-                        <div>
-                            <p>ISP</p>
-                            {
-                                this.state.address &&
-                                <h3>{this.state.address.isp}</h3>
-                            }
-                        </div>
+                <div className="container" onClick={this.handleClick}>
+                    <div className="data-cell">
+                        <p>IP ADDRESS</p>
+                        {
+                            this.state.address &&
+                            <h3> {this.state.address.ip}</h3>
+                        }
+                    </div>
+                    <div className="divider"></div>
+                    <div className="data-cell">
+                        <p>LOCATION</p>
+                        {
+                            this.state.address &&
+                            this.state.address.location &&
+                            <h3>
+                                {`${this.state.address.location.city}, `}
+                                {/* {`${this.state.address.location.region}, `} */}
+                                {`${this.state.address.location.country} `}
+                                {this.state.address.location.postalCode}
+                            </h3>
+                        }
+                    </div>
+                    <div className="divider"></div>
+                    <div className="data-cell">
+                        <p>TIMEZONE</p>
+                        {
+                            this.state.address &&
+                            this.state.address.location &&
+                            <h3>{`UTC ${this.state.address.location.timezone}`}</h3>
+                        }
+                    </div>
+                    <div className="divider"></div>
+                    <div className="data-cell">
+                        <p>ISP</p>
+                        {
+                            this.state.address &&
+                            <h3>{this.state.address.isp}</h3>
+                        }
                     </div>
                 </div>
             </section>
